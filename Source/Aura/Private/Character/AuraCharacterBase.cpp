e5bf914ea8528325eff2d4a265b9281256f3bc6d
ChrisBuckley1205 <chris.d.buckley@gmail.com>
2024-03-10 16:55:34 -0400

    Aura Character Blueprint and Goblin Spear Enemy BluePrint created with sockets in appropriate spots.

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,12 @@ AAuraCharacterBase::AAuraCharacterBase()
  	
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket")); // Attaching weapon to a socket, FName - we are creatng a socket with this Name and attaching it to the skeletal mesh that is inherited by the character class
+
+	// Weapon Shouldn't have any collision:
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 
